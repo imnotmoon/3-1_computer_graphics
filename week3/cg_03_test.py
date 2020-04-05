390d1e9bb3e3dec86e2d0e5769227cb8dc99f9fe
imnotmoon <imnotmoon@gmail.com>
2020-04-06 04:11:04 +0900

    Update cg_03_test.py
    1. 히스토그램 그리는거 잘됨. 정규화 이후에도 문제점 발견 못함..
    2. 역투영 직접코딩 + 기존 코드는 코멘트아웃
    pycharm 기준

--- week3/cg_03_test.py
+++ week3/cg_03_test.py
@@ -1,11 +1,10 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-# import sys, tty, termios        # 맥에서 엔터 없이 인풋받아오는게 안돼서..
 
 ########### 최적화는 이거 수정하면 될것 같습니다 #############
-_QUANTIZATED_HUE_ = 180              # 색조 : 70
-_QUANTIZATED_SATURATION_ = 256      # 채도 : 100
+_QUANTIZATED_HUE_ = 70              # 색조 : 70
+_QUANTIZATED_SATURATION_ = 100      # 채도 : 100
 _VAL_THRESHOLD_ = 50                # 임계값 : 50
 
 # 스페이스바 입력시 plt창을 끄게 만드는 함수입니다.
@@ -13,62 +12,56 @@ def quit_figure(event):
     if event.key == ' ':
         plt.close(event.canvas.figure)
 
-
 # 이미지 로드
 print("이미지 로드\n")
 model_image1 = cv2.imread('models.png')
-# model_image2 = cv2.imread('palette.png')
 target_image = cv2.imread('target.jfif')
 
 # HSV 변환
 print("HSV 변환\n")
 palette1 = cv2.cvtColor(model_image1, cv2.COLOR_BGR2HSV)
-# palette2 = cv2.cvtColor(model_image2, cv2.COLOR_BGR2HSV)
 hsvt = cv2.cvtColor(target_image, cv2.COLOR_BGR2HSV)
 
 # 오브젝트 히스토그램 추출 [H,S]
+# hist_roi = cv2.calcHist([palette1], [0, 1], None, [_QUANTIZATED_HUE_, _QUANTIZATED_SATURATION_], [0, 180, 0, 256])
 # 2-A
 print("오브젝트 히스토그램 추출 [H,S]\n")
+height, width = palette1.shape[:2]
+hist_roi=np.zeros((_QUANTIZATED_HUE_, _QUANTIZATED_SATURATION_))
+a=180/_QUANTIZATED_HUE_
+b=256/_QUANTIZATED_SATURATION_
+for j in range(height):     # 모든 픽셀을 탐색하여 히스토그램 생성하는 for문
+    for i in range(width):
+        h=int(palette1[j][i][0]/a)
+        s=int(palette1[j][i][1]/b)
+        hist_roi[h][s]+=1
 
-# 모든 픽셀을 탐색하여 히스토그램 생성하는 for문
-# height, width = palette1.shape[:2]
-# hist_roi=np.zeros((_QUANTIZATED_HUE_,_QUANTIZATED_SATURATION_),)
-# a=180/_QUANTIZATED_HUE_
-# b=256/_QUANTIZATED_SATURATION_
-#
-# for j in range(height):
-#     for i in range(width):
-#         h=int(palette1[j][i][0]/a)
-#         s=int(palette1[j][i][1]/b)
-#         hist_roi[h][s]+=1
+cv2.imshow('selfmade hist', hist_roi)
+cv2.waitKey(0)
 
-hist_roi = cv2.calcHist([palette1], [0, 1], None, [_QUANTIZATED_HUE_, _QUANTIZATED_SATURATION_], [0, 180, 0, 256])
-# hist_temp2 = cv2.calcHist([palette2], [0, 1], None, [_QUANTIZATED_HUE_, _QUANTIZATED_SATURATION_], [0, 180, 0, 256])
-# hist_roi = np.zeros((_QUANTIZATED_HUE_, _QUANTIZATED_SATURATION_))   # temp1, temp2를 합쳐서 여기 저장할 예정
-# print(np.shape(hist_temp1)[0], np.shape(hist_temp1)[1])
-
-
-# for i in range(_QUANTIZATED_HUE_):
-#     for j in range(_QUANTIZATED_SATURATION_) :
-#         hist_roi[i][j] = hist_temp1[i][j] + hist_temp2[i][j]
 
 # 2-C
 print("정규화한 히스토그램의 그래프\n")
-cv2.normalize(hist_roi, hist_roi, 0, 255, cv2.NORM_MINMAX)  # 출력용 정규화된 히스토그램 : HUE
-cv2.imshow('hist', hist_roi)
+cv2.normalize(hist_roi, hist_roi, 0, 255, cv2.NORM_MINMAX)
+print(hist_roi.shape[0], hist_roi.shape[1])
+cv2.imshow('normalized selfmade hist', hist_roi)
 cv2.waitKey(0)
 
 
-# 히스토그램 정규화 이후 대상 이미지에 역투영
+# 히스토그램 정규화 이후 대상 이미지에 역투영  # dst2 = cv2.calcBackProject([hsvt], [0, 1], hist_roi, [0, 180, 0, 256], 1)
 print("대상 이미지에 히스토그램 역투영\n")
-dst = cv2.calcBackProject([hsvt], [0, 1], hist_roi, [0, 180, 0, 256], 1)
+height, width = hsvt.shape[:2]
+dst = np.zeros((height, width), dtype = np.uint8)
+for i in range(height):
+    for j in range(width):
+        dst[i][j] = hist_roi[int(hsvt[i][j][0]/a)][int(hsvt[i][j][1]/b)]
 
 # 찾은 픽셀들 : 신뢰도 맵
 # 3-A
-cv2.imshow('dst', dst)
+cv2.imshow('backprojection', dst)
 cv2.waitKey(0)
 
-# 4-A       # cv2로 그래프 찍으니까 잘 안보임, range를 [0, 256]으로 하면 전부다 검은색(배경)이라 잘 안보여서 [1, 256]으로 함.
+# 4-A   # cv2로 그래프 찍으니까 잘 안보임, range를 [0, 256]으로 하면 전부다 검은색(배경)이라 잘 안보여서 [1, 256]으로 함.
 print("신뢰도 맵 히스토그램의 그래프\n")
 dst_hist = cv2.calcHist([dst], [0], None, [256], [1, 256])
 plt.plot(dst_hist)
@@ -81,7 +74,7 @@ disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
 cv2.filter2D(dst, -1, disc, dst)
 
 # # 찾은 픽셀들을 컨벌루션한거
-cv2.imshow('dst', dst)
+cv2.imshow('convoluted dst', dst)
 cv2.waitKey(0)
 
 
